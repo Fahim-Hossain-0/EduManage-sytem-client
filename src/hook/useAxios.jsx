25f87a4679eb201ff00ecc4d15d105da481296c7
fahim-hossain-0 <fahimhossain735@gmail.com>
2026-06-23 12:26:28 +0600

    update

--- src/hook/useAxios.jsx
+++ src/hook/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000
-  baseURL: "https://edu-manage-system-server.vercel.app",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 const useAxios = () => {
--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const axiosSecure = axios.create({
 //   baseURL: "http://localhost:5000",
-baseURL: "https://edu-manage-system-server.vercel.app",
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 const useAxiosSecure = () => {
